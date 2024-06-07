d9237cbbf6294b17ce042e9abf2b6ab5c5a19c78
Raghuram Aluru <raghuramalur22@gmail.com>
2024-06-07 18:53:52 +0530

    requiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'requiz';
+  showusr : boolean = false;
+  showdel : boolean = false;
+  userarr : {id : number , name : string, mnum : number , email : string} [] = [];
+  getuser(userdata : any){
+    let userob = {id : this.userarr.length,name : userdata.value.userName , mnum : userdata.value.mobile , email : userdata.value.email};
+    this.userarr.push(userob);
+    userdata.resetForm();
+  }
+  deluser(id : number){
+    this.userarr = this.userarr.filter(userdata=>userdata.id !== id);
+  }
+  tglusers(){
+    this.showusr = !this.showusr;
+  }
+  tgldelusers(){
+    this.showdel = !this.showdel;
+  }
 }
